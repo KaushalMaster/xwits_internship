1ee8b99451301ed4b0293d609df438f1974e6f77
Kaushal Soni <kaushal.sce20@sot.pdpu.ac.in>
2024-01-29 13:26:33 +0530

    completed with the concept of props

--- src/components/Expensess/ExpenseItem/ExpenseItem.jsx
+++ src/components/Expensess/ExpenseItem/ExpenseItem.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import "./ExpenseItem.css";
+import ExpenseDate from "../ExpenseDate/ExpenseDate";
+import Card from "../../UI/Card/Card";
 
 const ExpenseItem = (props) => {
   return (
     <div>
-      <div className="expense-item">
-        {props.date.toISOString()}
+      <Card className="expense-item">
+        <ExpenseDate date={props.date} />
         <div className="expense-item__description">
           <h2>{props.title}</h2>
           <div className="expense-item__price">${props.amount}</div>
         </div>
-      </div>
+      </Card>
     </div>
   );
 };
