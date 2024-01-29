0246069e206355518f75ff30176d0506d0bc1504
Kaushal Soni <kaushal.sce20@sot.pdpu.ac.in>
2024-01-29 15:37:45 +0530

    Method-1 to handle the form data using useState()

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -1,17 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+  const [title, setTitle] = useState("");
+  const [amount, setAmount] = useState("");
+  const [date, setDate] = useState("");
+
+  const titleChangeHandler = (event) => {
+    setTitle(event.target.value);
+    console.log(title);
+  };
+
+  const amountChangeHandler = (event) => {
+    setAmount(event.target.value);
+    console.log(amount);
+  };
+
+  const dateChangeHandler = (event) => {
+    setDate(event.target.value);
+    console.log(date);
+  };
+
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input type="text" onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={amountChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
