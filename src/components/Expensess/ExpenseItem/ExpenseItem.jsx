b2c33d9b460dd91bf262eed3ef8f20405b67b274
Kaushal Soni <kaushal.sce20@sot.pdpu.ac.in>
2024-01-29 14:27:03 +0530

    completed the state operation

--- src/components/Expensess/ExpenseItem/ExpenseItem.jsx
+++ src/components/Expensess/ExpenseItem/ExpenseItem.jsx
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "../ExpenseDate/ExpenseDate";
 import Card from "../../UI/Card/Card";
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle("Updated!");
+    console.log(title);
+  };
+
   return (
     <div>
       <Card className="expense-item">
         <ExpenseDate date={props.date} />
         <div className="expense-item__description">
-          <h2>{props.title}</h2>
+          <h2>{title}</h2>
           <div className="expense-item__price">${props.amount}</div>
         </div>
+        <button onClick={clickHandler}>Update</button>
       </Card>
     </div>
   );
